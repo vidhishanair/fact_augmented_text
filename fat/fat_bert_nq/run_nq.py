be6d0555a2ce8261f3f5ad78f3968c08d585cb51
Vidhisha Balachandran <vidhishanair@gmail.com>
2020-04-27 19:35:55 -0400

    iterating through google ents

--- fat/fat_bert_nq/run_nq.py
+++ fat/fat_bert_nq/run_nq.py
@@ -544,7 +544,12 @@ def create_example_from_jsonl(line):
   # annotated_sa: short answer start and end char offsets, (-1, -1) if null.
   question = {"input_text": e["question_text"], "entity_map": e["question_entity_map"]}
   if FLAGS.use_google_entities:
-      question['entity_map'].update(e['google_question_entity_map'])
+      for key, value in e['google_question_entity_map'].items():
+          if key in question['entity_map']:
+              question['entity_map'][key].extend(value)
+          else:
+              question['entity_map'][key] = value
+      # question['entity_map'].update(e['google_question_entity_map'])
   answer = {
       "candidate_id": annotated_idx,
       "span_text": "",
